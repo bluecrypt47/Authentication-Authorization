f03d13dd2769fd5a1e471611455cfcc3b195711a
bluecrypt47 <lechuong09291999@gmail.com>
2022-04-21 13:33:14 +0700

    add lab

---
+++ handle.php
@@ -0,0 +1,41 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+header('Content-Type: text/html; charset=utf-8');
+// Kết nối cơ sở dữ liệu
+$conn = mysqli_connect('localhost', 'root', '', 'xss') or die('Connection fail!');
+mysqli_set_charset($conn, "utf8");
+
+// Login
+if (isset($_POST['login'])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Two password do not match");
+    }
+
+    // Kiểm tra email và password có trong DB không
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+
+    // Thực thi câu truy vấn
+    $result = mysqli_query($conn, $sql);
+
+    // Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
+    if (mysqli_num_rows($result) > 0) {
+
+        echo '<script language="javascript">alert("Login Successfully!"); window.location="index.php";</script>';
+    } else {
+        echo '<script language="javascript">alert("Email has existed!"); window.location="login.php";</script>';
+        die();
+    }
+    $_SESSION['email'] = $email;
+    echo "Xin chào " . $username;
+    die();
+}
